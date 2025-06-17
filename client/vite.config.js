b57d24a199319fff4258c5bbe42ca097a5683e84
Ahamed_Meyan <116168645+Ahamed1846@users.noreply.github.com>
2025-06-18 01:59:41 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   preview: {
     host: '0.0.0.0',
-    port: 4173
-  }
+    port: 4173,
+    allowedHosts: ['shell-3imd.onrender.com']
+  },
 })
